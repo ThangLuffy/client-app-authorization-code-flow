6ec13d043da083b09899fe4bcfff562e33dc14e5
ThangLuffy <thangnv1002@gmail.com>
2025-12-23 16:20:47 +0700

    add config for webclient, OAuth2Client; refactor api integrate with resource server; change forms

--- src/main/java/com/example/clientapp/controller/HomeController.java
+++ src/main/java/com/example/clientapp/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.example.clientapp.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +19,8 @@ public class HomeController {
     }
 
     @GetMapping("/secured")
-    public String getUserName(Model model, Authentication authentication) {
-        model.addAttribute("username", authentication.getName());
+    public String loginSuccess(Model model, Authentication authentication) {
+        model.addAttribute("name", authentication.getName());
         return "secured";
     }
 
--- src/main/java/com/example/clientapp/controller/UserController.java
+++ src/main/java/com/example/clientapp/controller/UserController.java
@@ -1,49 +1,34 @@
 package com.example.clientapp.controller;
 
+import com.example.clientapp.model.Users;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ObjectUtils;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 @Controller
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
-//        TODO integrate with view
-//        TODO define config webclient, default set access token for header
-//        TODO define page view to display response from resource server
+    @Autowired
+    private WebClient webClient;
+
 //        TODO customize view of home page, login page
-    @GetMapping("/api/users/unit-test")
-    public String findUsers(@RegisteredOAuth2AuthorizedClient("keycloak") OAuth2AuthorizedClient client) {
-        log.info("Token from OAuth2AuthorizedClient: '{}'",
-                client != null && client.getAccessToken() != null ? client.getAccessToken().getTokenValue() : null);
-        String accessToken = client != null && client.getAccessToken() != null ? client.getAccessToken().getTokenValue() : null;
-        if (ObjectUtils.isEmpty(accessToken)) {
-            return "access token is empty in client application";
-        }
+    @GetMapping("/users")
+    public String findUsers(Model model) {
+        Users[] users = webClient.get()
+                .uri("http://localhost:8085/api/users")
+                .retrieve()
+                .bodyToMono(Users[].class)
+                .block();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-
-        HttpEntity<Void> entity = new HttpEntity<>(headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        ResponseEntity<String> response = restTemplate
-                .exchange(
-                        "http://localhost:8081/api/users",
-                        HttpMethod.GET,
-                        entity,
-                        String.class
-                );
-
-        return response.getBody();
+        log.info("List users from resource server '{}'", Objects.isNull(users) ? null : Arrays.toString(users));
+        model.addAttribute("users", users != null ? users : new Users[]{});
+        return "users";
     }
 }
